30d00dffe7b7355b0779a7877095cfc93f7fa09c
Arsh <120127753+ArshdevSinghji@users.noreply.github.com>
2025-09-14 17:40:23 +0530

    feat: implement event, order, inbox, and outbox message entities with necessary interfaces and repository methods

---
+++ sales/src/domain/order/order.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { OrderStatus } from './enum/order-status.enum';
+
+interface Product {
+  product_id: string;
+  quantity: number;
+}
+
+@Entity('order')
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'jsonb' })
+  products: Product[];
+
+  @Column({ type: 'uuid' })
+  customer_id: string;
+
+  @Column({ type: 'decimal' })
+  total_amount: number;
+
+  @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.PENDING })
+  status: OrderStatus;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+}
