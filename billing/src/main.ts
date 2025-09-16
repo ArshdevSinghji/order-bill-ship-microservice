cba6148fa7ec2f0c212fc9ffc7065419f16e9016
Arshdev <arshdevrajput@gmail.com>
2025-09-16 10:23:03 +0530

    feat(billing): initialize transactional context and update server port

--- billing/src/main.ts
+++ billing/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  addTransactionalDataSource,
+  initializeTransactionalContext,
+} from 'typeorm-transactional';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
+  initializeTransactionalContext();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const dataSource = app.get(DataSource);
+  addTransactionalDataSource(dataSource);
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
